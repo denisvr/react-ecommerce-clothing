d8a3c02dbd462ed9283e78970009eca33789eeea
denisvr <denisvr01@gmail.com>
2020-03-28 18:09:20 +0000

    Added basic router

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,7 +3,9 @@ import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
 
 const HomePage = () => (
-    <Directory/>
-)
+    <div className='homepage'>
+      <Directory />
+    </div>
+  );
 
 export default HomePage;
